f036671390f97d635bed654b59d7b7d43de63277
BDgDiem <jodd23855@gmail.com>
2024-07-23 14:42:16 +0700

    up api

---
+++ mongo/author.model.js
@@ -0,0 +1,13 @@
+// kết nối collection author
+
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const ObjectId = Schema.ObjectId; // khóa chính id
+
+const authorSchema = new Schema({
+  name: { type: String, require: true }, // require true là bắt buộc ---- fasle là ko bắt buộc
+  description: { type: String, require: true },
+});
+
+module.exports =
+  mongoose.models.author || mongoose.model("author", authorSchema); // kiển tra xem nó tồn tại cái model chưa, nếu chưa thì thêm vào
